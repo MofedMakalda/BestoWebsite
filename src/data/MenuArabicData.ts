0d326c8c9f11799b779b183464f6befbc65c8286
Mofedmakalda <mofedmk1@gmal.com>
2024-10-21 19:18:40 +0300

    changes

--- src/data/MenuArabicData.ts
+++ src/data/MenuArabicData.ts
@@ -1,4 +1,99 @@
-// Importing images
+// // Importing images
+// import specialBg from "../assets/background/special.jpg";
+// import burgerBg from "../assets/background/burger.jpg";
+// import drinksBg from "../assets/background/drink.jpg";
+// import sandwishBg from "../assets/background/sandwish.jpg";
+// import Appetizers from "../assets/background/Appetizers.png";
+// import Tortilla from "../assets/background/Tortilla.png";
+// import hotdog from "../assets/MainPic/sandwishes/hotdog.jpg";
+
+// // Define MenuItem and Category interfaces
+// export interface MenuItem {
+//   title: string;
+//   image: string;
+// }
+
+// export interface Category {
+//   title: string;
+//   items: MenuItem[];
+//   backgroundImage: string;
+// }
+
+// // Exporting the categories data
+// export const ArabicCategories: Category[] = [
+//   {
+//     title: "بكجات بيستو",
+//     items: [
+//       { title: "بكج  البروذرز (شخصين)", image: hotdog },
+//       { title: "بكج الشلة (٣ اشخاص)", image: hotdog },
+//       { title: "بكج عائلي (٤ اشخاص)", image: hotdog },
+//     ],
+//     backgroundImage: specialBg,
+//   },
+//   {
+//     title: "برچر ",
+//     items: [
+//       { title: "كلاسيك برچر 160 غرام", image: hotdog },
+//       { title: "سماش  برچر 200 غرام", image: hotdog },
+//       { title: "سماش  برچر 300 غرام", image: hotdog },
+//       { title: "برچر بيستو (بالفقع والبشاميل)", image: hotdog },
+//       { title: "برچر دجاج مقرمش", image: hotdog },
+//       { title: "برچر شنيتزل", image: hotdog },
+//     ],
+//     backgroundImage: burgerBg,
+//   },
+//   {
+//     title: "رول التورتيلا",
+//     items: [
+//       { title: "رول فيليه دجاج", image: hotdog },
+//       { title: "فيليه دجاج بالجبنة", image: hotdog },
+//       { title: "فيليه دجاج بالفقع", image: hotdog },
+//       { title: "رول شنيتسل", image: hotdog },
+
+//     ],
+//     backgroundImage: Tortilla,
+//   },
+//   {
+//     title: "الساندويتشات",
+//     items: [
+//       { title: "ساندويتش شنيتزل", image: hotdog },
+//       { title: "ساندويتش فيليه الدجاج", image: hotdog },
+//       { title: "ساندويتش البيض", image: hotdog },
+//       { title: "ساندويتش التونة", image: hotdog },
+//       { title: "ساندويتش جبنة صفرا", image: hotdog },
+//       { title: "ساندويتش جبنة بيضة", image: hotdog },
+//       { title: "ساندويتش بستراما", image: hotdog },
+//       { title: "ساندويتش نقنيق", image: hotdog },
+
+//     ],
+//     backgroundImage: sandwishBg,
+//   },
+//   {
+//     title: "المقبلات",
+//     items: [
+//       { title: "أجنحة الدجاج", image: hotdog },
+//       { title: "ناجتس بيستو ", image: hotdog },
+//       { title: "أعواد مقرمشة", image: hotdog },
+//       { title: "مكس ل 1", image: hotdog },
+//       { title: "مكس لشخصين", image: hotdog },
+//       { title: "مكس ثلاثي", image: hotdog },
+   
+//     ],
+//     backgroundImage: Appetizers,
+//   },
+
+
+
+//   {
+//     title: "مشروبات",
+//     items: [
+//       { title: "Cheesecake", image: hotdog },
+//       { title: "Chocolate", image: hotdog },
+//     ],
+//     backgroundImage: drinksBg,
+//   },
+// ];
+
 import specialBg from "../assets/background/special.jpg";
 import burgerBg from "../assets/background/burger.jpg";
 import drinksBg from "../assets/background/drink.jpg";
@@ -7,10 +102,33 @@ import Appetizers from "../assets/background/Appetizers.png";
 import Tortilla from "../assets/background/Tortilla.png";
 import hotdog from "../assets/MainPic/sandwishes/hotdog.jpg";
 
+//Burgers
+//MushroomBurger
+import mushroomBurgerMain from "../assets/MainPic/Mushroomburger/Main.png"
+import mushroomBurger1 from "../assets/MainPic/Mushroomburger/1.jpg"
+import mushroomBurger2 from "../assets/MainPic/Mushroomburger/2.jpg"
+import mushroomBurger3 from "../assets/MainPic/Mushroomburger/3.jpg"
+import mushroomBurger4 from "../assets/MainPic/Mushroomburger/4.jpg"
+import mushroomBurger5 from "../assets/MainPic/Mushroomburger/5.jpg"
+import mushroomBurger6 from "../assets/MainPic/Mushroomburger/6.jpg"
+
+//Shnitzel Burger
+import shnitselBurgerMain from "../assets/MainPic/shnetselBurger/main.png"
+import shnetselBurger1 from "../assets/MainPic/shnetselBurger/1.jpg"
+import shnetselBurger2 from "../assets/MainPic/shnetselBurger/2.jpg"
+import shnetselBurger3 from "../assets/MainPic/shnetselBurger/3.jpg"
+import shnetselBurger4 from "../assets/MainPic/shnetselBurger/4.jpg"
+import shnetselBurger5 from "../assets/MainPic/shnetselBurger/5.jpg"
+import shnetselBurger6 from "../assets/MainPic/shnetselBurger/6.jpg"
+import shnetselBurger7 from "../assets/MainPic/shnetselBurger/7.jpg"
+
+
+
+
 // Define MenuItem and Category interfaces
 export interface MenuItem {
   title: string;
-  image: string;
+  images: string[];
 }
 
 export interface Category {
@@ -24,31 +142,31 @@ export const ArabicCategories: Category[] = [
   {
     title: "بكجات بيستو",
     items: [
-      { title: "بكج  البروذرز (شخصين)", image: hotdog },
-      { title: "بكج الشلة (٣ اشخاص)", image: hotdog },
-      { title: "بكج عائلي (٤ اشخاص)", image: hotdog },
+      { title: "بكج  البروذرز (شخصين)", images: [hotdog] },
+      { title: "بكج الشلة (٣ اشخاص)", images: [hotdog] },
+      { title: "بكج عائلي (٤ اشخاص)",images: [hotdog] },
     ],
     backgroundImage: specialBg,
   },
   {
     title: "برچر ",
     items: [
-      { title: "كلاسيك برچر 160 غرام", image: hotdog },
-      { title: "سماش  برچر 200 غرام", image: hotdog },
-      { title: "سماش  برچر 300 غرام", image: hotdog },
-      { title: "برچر بيستو (بالفقع والبشاميل)", image: hotdog },
-      { title: "برچر دجاج مقرمش", image: hotdog },
-      { title: "برچر شنيتزل", image: hotdog },
+      { title: "كلاسيك برچر 160 غرام", images: [hotdog] },
+      { title: "سماش  برچر 200 غرام", images: [hotdog] },
+      { title: "سماش  برچر 300 غرام", images: [hotdog] },
+      { title: "برچر بيستو (بالفقع والبشاميل)", images: [mushroomBurgerMain,mushroomBurger1, mushroomBurger2, mushroomBurger3, mushroomBurger4,mushroomBurger5, mushroomBurger6 ] },
+      { title: "برچر دجاج مقرمش", images: [hotdog] },
+      { title: "برچر شنيتزل", images: [shnitselBurgerMain,shnetselBurger1,shnetselBurger2,shnetselBurger3,shnetselBurger4,shnetselBurger5, shnetselBurger6,shnetselBurger7] },
     ],
     backgroundImage: burgerBg,
   },
   {
     title: "رول التورتيلا",
     items: [
-      { title: "رول فيليه دجاج", image: hotdog },
-      { title: "فيليه دجاج بالجبنة", image: hotdog },
-      { title: "فيليه دجاج بالفقع", image: hotdog },
-      { title: "رول شنيتسل", image: hotdog },
+      { title: "رول فيليه دجاج", images: [hotdog] },
+      { title: "فيليه دجاج بالجبنة", images: [hotdog] },
+      { title: "فيليه دجاج بالفقع", images: [hotdog] },
+      { title: "رول شنيتسل", images: [hotdog] },
 
     ],
     backgroundImage: Tortilla,
@@ -56,14 +174,14 @@ export const ArabicCategories: Category[] = [
   {
     title: "الساندويتشات",
     items: [
-      { title: "ساندويتش شنيتزل", image: hotdog },
-      { title: "ساندويتش فيليه الدجاج", image: hotdog },
-      { title: "ساندويتش البيض", image: hotdog },
-      { title: "ساندويتش التونة", image: hotdog },
-      { title: "ساندويتش جبنة صفرا", image: hotdog },
-      { title: "ساندويتش جبنة بيضة", image: hotdog },
-      { title: "ساندويتش بستراما", image: hotdog },
-      { title: "ساندويتش نقنيق", image: hotdog },
+      { title: "ساندويتش شنيتزل", images: [hotdog] },
+      { title: "ساندويتش فيليه الدجاج",images: [hotdog] },
+      { title: "ساندويتش البيض", images: [hotdog] },
+      { title: "ساندويتش التونة", images: [hotdog] },
+      { title: "ساندويتش جبنة صفرا", images: [hotdog] },
+      { title: "ساندويتش جبنة بيضة", images: [hotdog] },
+      { title: "ساندويتش بستراما",images: [hotdog] },
+      { title: "ساندويتش نقنيق", images: [hotdog] },
 
     ],
     backgroundImage: sandwishBg,
@@ -71,12 +189,12 @@ export const ArabicCategories: Category[] = [
   {
     title: "المقبلات",
     items: [
-      { title: "أجنحة الدجاج", image: hotdog },
-      { title: "ناجتس بيستو ", image: hotdog },
-      { title: "أعواد مقرمشة", image: hotdog },
-      { title: "مكس ل 1", image: hotdog },
-      { title: "مكس لشخصين", image: hotdog },
-      { title: "مكس ثلاثي", image: hotdog },
+      { title: "أجنحة الدجاج", images: [hotdog] },
+      { title: "ناجتس بيستو ", images: [hotdog] },
+      { title: "أعواد مقرمشة", images: [hotdog] },
+      { title: "مكس ل 1", images: [hotdog] },
+      { title: "مكس لشخصين", images: [hotdog] },
+      { title: "مكس ثلاثي", images: [hotdog] },
    
     ],
     backgroundImage: Appetizers,
@@ -87,8 +205,8 @@ export const ArabicCategories: Category[] = [
   {
     title: "مشروبات",
     items: [
-      { title: "Cheesecake", image: hotdog },
-      { title: "Chocolate", image: hotdog },
+      { title: "CocaCola",images: [hotdog] },
+      { title: "Sprite", images: [hotdog] },
     ],
     backgroundImage: drinksBg,
   },
--- src/data/MenuEnglishData.ts
+++ src/data/MenuEnglishData.ts
@@ -1,3 +1,97 @@
+// // Importing images
+// import specialBg from "../assets/background/special.jpg";
+// import burgerBg from "../assets/background/burger.jpg";
+// import drinksBg from "../assets/background/drink.jpg";
+// import sandwishBg from "../assets/background/sandwish.jpg";
+// import Appetizers from "../assets/background/Appetizers.png";
+// import Tortilla from "../assets/background/Tortilla.png";
+// import hotdog from "../assets/MainPic/sandwishes/hotdog.jpg";
+
+
+// // Define MenuItem and Category interfaces
+// export interface MenuItem {
+//   title: string;
+//   image: string;
+// }
+
+// export interface Category {
+//   title: string;
+//   items: MenuItem[];
+//   backgroundImage: string;
+// }
+
+// // Exporting the categories data
+// export const EnglishCategories: Category[] = [
+//   {
+//     title: "Besto’s specials",
+//     items: [
+//       { title: "Brothers Package (2 people)", image: hotdog },
+//       { title: "Friends Package (3 people)", image: hotdog },
+//       { title: "Family Package (4 people)", image: hotdog },
+
+//     ],
+//     backgroundImage: specialBg,
+//   },
+//   {
+//     title: "Burgers",
+//     items: [
+//       { title: "Besto's Burger (With mushrooms)", image: hotdog },
+//       { title: "Classic Burger 160g", image: hotdog },
+//       { title: "Smash Burger 200g", image: hotdog },
+//       { title: "Smash Burger 300g", image: hotdog },
+//       { title: "Crispy chicken burger ", image: hotdog },
+//       { title: "Shnitzel burger", image: hotdog },
+
+//     ],
+//     backgroundImage: burgerBg,
+//   },
+//   {
+//     title: "Tortilla roll",
+//     items: [
+//       { title: "Chick fillet", image: hotdog },
+//       { title: "Cheese chick fillet", image: hotdog },
+//       { title: "Mushroom chick fillet", image: hotdog },
+//       { title: "Shnitzel roll", image: hotdog },
+
+//     ],
+//     backgroundImage: Tortilla,
+//   },
+//   {
+//     title: "Sandwiches",
+//     items: [
+//       { title: "Shnitzel Sandwich ", image: hotdog },
+//       { title: "Chick fillet Sandwich ", image: hotdog },
+//       { title: "Omlet Sandwich ", image: hotdog },
+//       { title: "Tuna Sandwich ", image: hotdog },
+//       { title: "Yellow Cheese Sandwich ", image: hotdog },
+//       { title: "White cheese Sandwich ", image: hotdog },
+//       { title: "Pastrami Sandwich ", image: hotdog },
+//       { title: "Salami Sandwich ", image: hotdog },
+//     ],
+//     backgroundImage: sandwishBg,
+//   },
+//   {
+//     title: "Appetizers",
+//     items: [
+//       { title: "Chicken wings", image: hotdog },
+//       { title: "Besto’s Nuggets", image: hotdog },
+//       { title: "Crispy sticks", image: hotdog },
+//       { title: "Mix for 1", image: hotdog },
+//       { title: "Dbl mix (for 2)", image: hotdog },
+//       { title: "Trio mix(for 3)", image: hotdog },
+   
+//     ],
+//     backgroundImage: Appetizers,
+//   },
+//   {
+//     title: "Drinks",
+//     items: [
+//       { title: "Cheesecake", image: hotdog },
+//       { title: "Chocolate", image: hotdog },
+//     ],
+//     backgroundImage: drinksBg,
+//   },
+// ];
 // Importing images
 import specialBg from "../assets/background/special.jpg";
 import burgerBg from "../assets/background/burger.jpg";
@@ -7,11 +101,33 @@ import Appetizers from "../assets/background/Appetizers.png";
 import Tortilla from "../assets/background/Tortilla.png";
 import hotdog from "../assets/MainPic/sandwishes/hotdog.jpg";
 
+//Burgers
+//MushroomBurger
+import mushroomBurgerMain from "../assets/MainPic/Mushroomburger/Main.png"
+import mushroomBurger1 from "../assets/MainPic/Mushroomburger/1.jpg"
+import mushroomBurger2 from "../assets/MainPic/Mushroomburger/2.jpg"
+import mushroomBurger3 from "../assets/MainPic/Mushroomburger/3.jpg"
+import mushroomBurger4 from "../assets/MainPic/Mushroomburger/4.jpg"
+import mushroomBurger5 from "../assets/MainPic/Mushroomburger/5.jpg"
+import mushroomBurger6 from "../assets/MainPic/Mushroomburger/6.jpg"
+
+//Shnitzel Burger
+import shnitselBurgerMain from "../assets/MainPic/shnetselBurger/main.png"
+import shnetselBurger1 from "../assets/MainPic/shnetselBurger/1.jpg"
+import shnetselBurger2 from "../assets/MainPic/shnetselBurger/2.jpg"
+import shnetselBurger3 from "../assets/MainPic/shnetselBurger/3.jpg"
+import shnetselBurger4 from "../assets/MainPic/shnetselBurger/4.jpg"
+import shnetselBurger5 from "../assets/MainPic/shnetselBurger/5.jpg"
+import shnetselBurger6 from "../assets/MainPic/shnetselBurger/6.jpg"
+import shnetselBurger7 from "../assets/MainPic/shnetselBurger/7.jpg"
+
+
+
 
 // Define MenuItem and Category interfaces
 export interface MenuItem {
   title: string;
-  image: string;
+  images: string[];
 }
 
 export interface Category {
@@ -25,69 +141,65 @@ export const EnglishCategories: Category[] = [
   {
     title: "Besto’s specials",
     items: [
-      { title: "Brothers Package (2 people)", image: hotdog },
-      { title: "Friends Package (3 people)", image: hotdog },
-      { title: "Family Package (4 people)", image: hotdog },
-
+      { title: "Brothers Package (2 people)", images: [hotdog] },
+      { title: "Friends Package (3 people)", images: [hotdog] },
+      { title: "Family Package (4 people)", images: [hotdog] },
     ],
     backgroundImage: specialBg,
   },
   {
     title: "Burgers",
     items: [
-      { title: "Besto's Burger (With mushrooms)", image: hotdog },
-      { title: "Classic Burger 160g", image: hotdog },
-      { title: "Smash Burger 200g", image: hotdog },
-      { title: "Smash Burger 300g", image: hotdog },
-      { title: "Crispy chicken burger ", image: hotdog },
-      { title: "Shnitzel burger", image: hotdog },
-
+      { title: "Besto's Burger (With mushrooms)", images: [mushroomBurgerMain,mushroomBurger1, mushroomBurger2, mushroomBurger3, mushroomBurger4,mushroomBurger5, mushroomBurger6 ] },
+      { title: "Classic Burger 160g", images: [hotdog] },
+      { title: "Smash Burger 200g", images: [hotdog] },
+      { title: "Smash Burger 300g", images: [hotdog] },
+      { title: "Crispy chicken burger", images: [hotdog] },
+      { title: "Shnitzel burger", images: [shnitselBurgerMain,shnetselBurger1,shnetselBurger2,shnetselBurger3,shnetselBurger4,shnetselBurger5, shnetselBurger6,shnetselBurger7] },
     ],
     backgroundImage: burgerBg,
   },
   {
     title: "Tortilla roll",
     items: [
-      { title: "Chick fillet", image: hotdog },
-      { title: "Cheese chick fillet", image: hotdog },
-      { title: "Mushroom chick fillet", image: hotdog },
-      { title: "Shnitzel roll", image: hotdog },
-
+      { title: "Chick fillet", images: [hotdog] },
+      { title: "Cheese chick fillet", images: [hotdog] },
+      { title: "Mushroom chick fillet", images: [hotdog] },
+      { title: "Shnitzel roll", images: [hotdog] },
     ],
     backgroundImage: Tortilla,
   },
   {
     title: "Sandwiches",
     items: [
-      { title: "Shnitzel Sandwich ", image: hotdog },
-      { title: "Chick fillet Sandwich ", image: hotdog },
-      { title: "Omlet Sandwich ", image: hotdog },
-      { title: "Tuna Sandwich ", image: hotdog },
-      { title: "Yellow Cheese Sandwich ", image: hotdog },
-      { title: "White cheese Sandwich ", image: hotdog },
-      { title: "Pastrami Sandwich ", image: hotdog },
-      { title: "Salami Sandwich ", image: hotdog },
+      { title: "Shnitzel Sandwich", images: [hotdog] },
+      { title: "Chick fillet Sandwich", images: [hotdog] },
+      { title: "Omlet Sandwich", images: [hotdog] },
+      { title: "Tuna Sandwich", images: [hotdog] },
+      { title: "Yellow Cheese Sandwich", images: [hotdog] },
+      { title: "White cheese Sandwich", images: [hotdog] },
+      { title: "Pastrami Sandwich", images: [hotdog] },
+      { title: "Salami Sandwich", images: [hotdog] },
     ],
     backgroundImage: sandwishBg,
   },
   {
     title: "Appetizers",
     items: [
-      { title: "Chicken wings", image: hotdog },
-      { title: "Besto’s Nuggets", image: hotdog },
-      { title: "Crispy sticks", image: hotdog },
-      { title: "Mix for 1", image: hotdog },
-      { title: "Dbl mix (for 2)", image: hotdog },
-      { title: "Trio mix(for 3)", image: hotdog },
-   
+      { title: "Chicken wings", images: [hotdog] },
+      { title: "Besto’s Nuggets", images: [hotdog] },
+      { title: "Crispy sticks", images: [hotdog] },
+      { title: "Mix for 1", images: [hotdog, hotdog] },
+      { title: "Dbl mix (for 2)", images: [hotdog] },
+      { title: "Trio mix (for 3)", images: [hotdog] },
     ],
     backgroundImage: Appetizers,
   },
   {
     title: "Drinks",
     items: [
-      { title: "Cheesecake", image: hotdog },
-      { title: "Chocolate", image: hotdog },
+      { title: "Cheesecake", images: [hotdog] },
+      { title: "Chocolate", images: [hotdog] },
     ],
     backgroundImage: drinksBg,
   },
